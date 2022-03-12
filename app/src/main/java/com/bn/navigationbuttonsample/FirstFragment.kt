6d3445e00a80a274fa7ef33246a78ebdb200cf31
B.N <19542357+BNSC1@users.noreply.github.com>
2022-03-12 10:27:24 +0800

    - Add: destination change listener.

--- app/src/main/java/com/bn/navigationbuttonsample/FirstFragment.kt
+++ app/src/main/java/com/bn/navigationbuttonsample/FirstFragment.kt
@@ -5,5 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.findNavController
+import com.bn.navigationbuttonsample.databinding.FragmentFirstBinding
 
-class FirstFragment : Fragment()
+class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_to_secondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/bn/navigationbuttonsample/MainActivity.kt
+++ app/src/main/java/com/bn/navigationbuttonsample/MainActivity.kt
@@ -3,8 +3,10 @@ package com.bn.navigationbuttonsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.navigation.NavController
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.bn.navigationbuttonsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,14 +15,57 @@ class MainActivity : AppCompatActivity() {
     private val navHostFragment by lazy {
         supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
     }
-    private val navController: NavController get() = navHostFragment.navController
+    private val navController get() = navHostFragment.navController
+    private var isRootFragment = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         with(binding) {
             setContentView(root)
-            setSupportActionBar(toolbar)
+            setupToolbar()
         }
     }
+
+    private fun ActivityMainBinding.setupToolbar() {
+        setSupportActionBar(toolbar)
+        NavigationUI.setupActionBarWithNavController(
+            this@MainActivity,
+            navController,
+            layoutDrawer
+        )
+        toggle = ActionBarDrawerToggle(
+            this@MainActivity, layoutDrawer,
+            toolbar, R.string.action_open, R.string.action_close
+        )
+        toggle.syncState()
+        layoutDrawer.addDrawerListener(toggle)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            isRootFragment = when (destination.id) {
+                R.id.firstFragment -> true
+                else -> false
+            }
+            updateDrawerNavigation()
+        }
+    }
+
+    private fun ActivityMainBinding.updateDrawerNavigation() {
+        supportActionBar?.let { actionBar ->
+            if (isRootFragment) {
+                actionBar.setDisplayHomeAsUpEnabled(false)
+                toggle.syncState()
+                toolbar.setNavigationOnClickListener {
+                    layoutDrawer.openDrawer(
+                        GravityCompat.START
+                    )
+                }
+                layoutDrawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNDEFINED)
+            } else {
+                layoutDrawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+                actionBar.setDisplayHomeAsUpEnabled(true)
+                toolbar.setNavigationOnClickListener { onBackPressed() }
+            }
+        }
+    }
+
 }
